ac945b64b686e4976cfc30877903bb6c81477334
safikulalam9679 <safikulalam9679@gmail.com>
2024-04-04 03:48:05 +0530

    updated oops

--- employees.java
+++ employees.java
@@ -6,7 +6,7 @@ class Employee{
     int salary;
 
     
-    public Employee(String name,int age,long phone,String address,int salary){
+    Employee(String name,int age,long phone,String address,int salary){
         this.name=name;
         this.age=age;
         this.phone=phone;
@@ -14,12 +14,12 @@ class Employee{
         this.salary=salary;
     }
     void printsalary(){
-        System.out.print("salary: " + salary);
+        System.out.println("salary: " + salary);
     }
 }
 class Officer extends Employee{
     String specialization;
-    public Officer(String name, int age, int phone, String address, int salary,String special) {
+    Officer(String name, int age, int phone, String address, int salary,String special) {
         super(name,age,phone,address,salary);
         this.specialization=special;
     }
@@ -28,7 +28,7 @@ class Officer extends Employee{
 }
 class Manager extends Employee{
     String department;
-   public Manager(String name, int age, int phone, String address, int salary,String app){
+    Manager(String name, int age, int phone, String address, int salary,String app){
     super(name,age,phone,address,salary);
     this.department=app;
     }
@@ -37,11 +37,8 @@ class Manager extends Employee{
 public class employees {
     public static void main(String[] args) {
         Officer off = new Officer("shamim",29,98003,"simhat",20000,"IT");
-        // off.printsalary();
+        
         Manager man= new Manager("faiz",20,963258,"haringhata",26530,"bca");
-        // man.printsalary();
-
-
 
         System.out.println("Officer Details:");
         System.out.println("Name: " + off.name);
